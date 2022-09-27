23dd13318ade5b1892407073ee8bec4de61853be
gerasaka <gerryjulioo@gmail.com>
2022-09-27 20:43:45 +0700

    feat: create function  to close edit modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
+// TODO: refactor wrapper and modal
+
 document.addEventListener('DOMContentLoaded', () => {
   loadData();
 });
 
 const openFormModal = document.getElementById('open-form-modal');
 const closeFormModal = document.getElementById('close-form-modal');
-const closeDetailModal = document.getElementById('close-details-modal');
+const closeDetailsModal = document.getElementById('close-details-modal');
+const closeEditModal = document.getElementById('close-edit-modal');
 
 openFormModal.addEventListener('click', () => {
   document.getElementById('form-wrapper').style.display = 'block';
@@ -14,12 +17,19 @@ closeFormModal.addEventListener('click', () => {
   document.getElementById('form-wrapper').style.display = 'none';
 });
 
-closeDetailModal.addEventListener('click', () => {
+closeDetailsModal.addEventListener('click', () => {
   document.getElementById('details-wrapper').style.display = 'none';
+});
+
+closeEditModal.addEventListener('click', () => {
+  editWrapper.style.display = 'none';
 });
 
 const addBookForm = document.getElementById('add-book');
 const searchButton = document.getElementById('search');
+const detailsModal = document.getElementById('details-wrapper');
+const editWrapper = document.getElementById('edit-wrapper');
+const editForm = document.getElementById('edit-book');
 
 addBookForm.addEventListener('submit', e => saveBook(e));
 searchButton.addEventListener('click', e => searchBook(e));
